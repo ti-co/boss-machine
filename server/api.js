4bba3a04e3cc83890007897fa65b3b6e60f76eb2
ti-co <kristof_welslau@hotmail.com>
2021-06-14 12:53:31 +0200

    try withour apiRouter.use ideas, meetings and minions

--- server/api.js
+++ server/api.js
@@ -3,14 +3,7 @@ const apiRouter = express.Router();
 
 apiRouter.use(express.static('public'));
 
-const minionsRouter = require('./minions');
-const ideasRouter = require('./ideas');
-const meetingsRouter = require('./meetings');
 
-
-apiRouter.use('/minions', minionsRouter); 
-apiRouter.use('/ideas', ideasRouter);
-apiRouter.use('/meetings', meetingsRouter);
 
 
 module.exports = apiRouter;
