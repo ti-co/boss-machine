3ef7f77dfdf1aee37c13170b894ac1faf20eba0c
ti-co <kristof_welslau@hotmail.com>
2021-06-14 18:19:18 +0200

    syntax error

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.use(bodyParser.json())
 const apiRouter = require('./server/api');
 app.use('/api', apiRouter);
 
-if(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'browser/build')));
+if(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'browser/build')))};
 
-app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/public/index.html'));})
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/public/index.html'))})
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
